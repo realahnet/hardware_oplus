9e2e894c8e5d73161062a2364fdadf55f0311dd9
Hecheng Yu <kde-yyds@qq.com>
2026-06-08 14:30:51 +0800

    sepolicy: Label libapsfixup.so
    06-08 14:05:22.329  2149  2149 W binder:2149_1: type=1400 audit(0.0:53): avc:  denied  { read } for  name=libapsfixup.so dev=dm-0 ino=73732591 scontext=u:r:cameraserver:s0 tcontext=u:object_r:vendor_file:s0 tclass=file permissive=0
    06-08 14:05:22.336  2149  2279 E vndksupport: Could not load odm/lib64/libAlgoProcess.so from sphal namespace: dlopen failed: library libapsfixup.so not found: needed by /odm/lib64/libAlgoProcess.so in namespace sphal.
    
    * Import oplus-fwk from: https://github.com/dodge-camera-port/android_hardware_oplus/tree/16.0/oplus-fwk
    * Proper commits with authorship will be done later once the cam is out of debugging state

---
+++ oplus-fwk/src/com/oplus/osense/OsenseResEventClient.java
@@ -0,0 +1,33 @@
+package com.oplus.osense;
+
+import com.oplus.osense.eventinfo.EventConfig;
+import com.oplus.osense.eventinfo.OsenseEventCallback;
+import android.os.Bundle;
+
+public class OsenseResEventClient {
+
+    private static OsenseResEventClient sInstance;
+
+    public static OsenseResEventClient getInstance() {
+        if (sInstance == null) {
+            sInstance = new OsenseResEventClient();
+        }
+        return sInstance;
+    }
+
+    public int registerEventCallback(OsenseEventCallback callback, EventConfig eventConfig) {
+        return 0;
+    }
+
+    public int unregisterEventCallback(OsenseEventCallback callback, EventConfig eventConfig) {
+        return 0;
+    }
+
+    public void requestSceneAction(Bundle bundle) {
+        return;
+    }
+
+    public int requestInstantCpuLoad() {
+        return 0;
+    }
+}
